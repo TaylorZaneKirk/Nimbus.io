18dce34362dc118c1a3547618cc5932fdd365c86
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-07 12:21:56 -0600

    Whoops

--- PHP-code/phpstuff.php
+++ PHP-code/phpstuff.php
@@ -1,18 +1,38 @@
-if($preparedQuery = $dbConnect->prepare('SELECT balance FROM accounts WHERE account_id=?')){
-                        $preparedQuery->bind_param('s', $account);  //'?' from above query becomes $account
-                        $preparedQuery->execute();  //send the query to db
-                        $preparedQuery->bind_result($balance);  //the result of the query can
-                                                                //ONLY be placed in $balance
-                        //get result of query
-                        if($preparedQuery->fetch()){
-                            //fetch was good, do the displays
-                            //List working-account
-                            echo 'Current Account: ' . $account . '<br>';
-                            //Display balance of the account to the user
-                            echo 'Your Current Balance: $' . $balance;
-                        }
-                        else{
-                            //input was good, but record does not exist in db
-                            echo 'Request Refused: Please check your Account Number and try again';
-                        }
-                        $preparedQuery->close();    //secure close
+<?php
+	require "conn.php";
+	if($preparedQuery = $conn->prepare('SELECT * FROM userlogin WHERE username=:uname AND password=:upass')){
+		$preparedQuery->bind_param('uname', $user_name);  //'?' from above query becomes $account
+		$preparedQuery->bind_param('upass', $user_pass);
+		$preparedQuery->execute();  //send the query to db
+		$preparedQuery->bind_result($user_data);  //the result of the query can
+												//ONLY be placed in $user_data, which needs to be shared between all activities
+		//get result of query
+		if($preparedQuery->fetch()){
+			//fetch was good, do the displays
+			//List working-account
+			echo 'login success !!!!! Welcome ' . $user_name;
+		}
+		else{
+			//input was good, but record does not exist in db
+			echo 'Request Refused: Please check your username or password and try again';
+		}
+		$preparedQuery->close();    //secure close
+?>
+
+<?php
+	////////////////////
+	//Connect to Server
+	///////////////////
+	//Server creditials
+	$host = 'localhost';
+	$db   = 'nimbus_android';
+	$user = 'nimbus_android';
+	$pass = 'nimbus.io'; //please do not hack me, Passos
+	$dbConnect = new mysqli($host, $user, $pass, $db);  //attempt to connect to db
+	
+	if(mysqli_connect_errno()){
+		//Could not establish a connection to the database
+		echo 'No Server Response';
+		exit;
+	}
+?>
