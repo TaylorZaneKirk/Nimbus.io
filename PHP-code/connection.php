594ef05b553bf9dfe105f8d6add9a3b80d564cb8
Vishnu Chaitanya Mandalapu <vishnuchaitanyam@gmail.com>
2016-11-07 11:46:23 -0600

    Create connection.php

---
+++ PHP-code/connection.php
@@ -0,0 +1,9 @@
+
+<?php 
+$db_name = "nimbus_android";
+$mysql_username = "nimbus_android";
+$mysql_password = "nimbus.io";
+$server_name = "localhost";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password,$db_name);
+ 
+?>
