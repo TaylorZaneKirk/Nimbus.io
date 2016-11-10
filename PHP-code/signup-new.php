85a199712942b19bc626a5c1b081b3fd89cbadb9
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-10 12:05:34 -0600

    Prepared version of signup script. I have not tested it yet, but should work

---
+++ PHP-code/signup-new.php
@@ -0,0 +1,21 @@
+<?php
+    require "conn.php";
+
+    $fname = $_POST["first_name"];
+    $lname = $_POST["last_name"];
+    $uname = $_POST["user_name"];
+    $upass = $_POST["password"];
+    $uemail = $_POST["user_email"];
+    $uphone = $_POST["user_phone"];
+
+    if($preparedQuery = $conn->prepare('INSERT into nimbusiouser (username, firstname, lastname, password, email, phone) VALUES (?, ?, ?, ?, ?, ?)')){
+            $preparedQuery->bind_param('ssssss', $uname, $fname, $lname, $upass, $uemail, $uphone); 
+            $preparedQuery->execute();
+
+            echo 'Row inserted';
+    }
+    else{
+        "Error: ".$conn->error;
+    }
+    $conn.close();
+?>
