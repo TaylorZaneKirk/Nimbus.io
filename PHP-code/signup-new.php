5feeb007ed4dd7de2109350628344b340aa402ca
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-11 19:46:06 -0600

    added checking if an account already exists before creating a new one

--- PHP-code/signup-new.php
+++ PHP-code/signup-new.php
@@ -8,14 +8,29 @@
     $uemail = $_POST["user_email"];
     $uphone = $_POST["user_phone"];
 
-    if($preparedQuery = $conn->prepare('INSERT into nimbusiouser (username, firstname, lastname, password, email, phone) VALUES (?, ?, ?, ?, ?, ?)')){
-            $preparedQuery->bind_param('ssssss', $uname, $fname, $lname, $upass, $uemail, $uphone); 
-            $preparedQuery->execute();
+    //check if there is already an account associated with specified username or email
+    if($preparedQuery = $conn->prepare('SELECT * FROM numbusiouser WHERE username=? OR email=?')){
+        $preparedQuery->bind_param('ss', $uname, $uemail);
+        $preparedQuery->execute();
+        $preparedQuery->bind_result($matches);
 
-            echo 'Row inserted';
+        if($preparedQuery->fetch()){
+            //match found, refuse
+            echo 'Error: Account with your chosen username or email already exists!';
+        }
+        else{
+            //No record exists, add one...
+            if($preparedQuery = $conn->prepare('INSERT into nimbusiouser (username, firstname, lastname, password, email, phone) VALUES (?, ?, ?, ?, ?, ?)')){
+                $preparedQuery->bind_param('ssssss', $uname, $fname, $lname, $upass, $uemail, $uphone); 
+                $preparedQuery->execute();
+
+                echo 'Row inserted';
+            }
+            else{
+                echo "Error: ".$conn->error;
+            }
+        }
     }
-    else{
-        echo "Error: ".$conn->error;
-    }
+    
     $conn.close();
 ?>
