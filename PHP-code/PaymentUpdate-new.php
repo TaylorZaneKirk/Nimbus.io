7d0da4d8d72463ac72f493535b9b314fc6243b7c
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-26 12:35:47 -0600

    Fixed uid discrepency

--- PHP-code/PaymentUpdate-new.php
+++ PHP-code/PaymentUpdate-new.php
@@ -1,7 +1,7 @@
 <?php
     require "conn.php";
 
-    $ccuid = $_POST["cc_uid"]; //REMOVE THIS!!! uid in db table MUST be set to auto-increment
+    $ccuid = $_POST["cc_uid"];
     $ccn= $_POST["cc_number"];
     $ccun= $_POST["cc_name"];
     $cced= $_POST["cc_exp"];
@@ -11,8 +11,8 @@
     $ccadd2 = $_POST["cc_add2"];
     $cczip = $_POST["cc_zip"];
 
-    if($preparedQuery = $conn->prepare('SELECT * FROM nimbusiouserpayment WHERE nameoncard=? AND cardnumber=?')){
-        $preparedQuery->bind_param('si', $ccun, $ccn);
+    if($preparedQuery = $conn->prepare('SELECT * FROM nimbusiouserpayment WHERE uid=? AND cardnumber=?')){
+        $preparedQuery->bind_param('ii', $ccuid, $ccn);
         $preparedQuery->execute();
 
         if($preparedQuery->fetch()){
@@ -20,8 +20,8 @@
             echo 'Error: This card already exists!';
         }
         else{
-             if($preparedQuery = $conn->prepare('INSERT into nimbusiouserpayment (cardnumber, nameoncard, expdate, ccissuer, cvv, addressl1, addressl2, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)')){
-                $preparedQuery->bind_param('isssissi', $ccn, $ccun, $cced, $cci, $cccvv, $ccadd1, $ccadd2, $cczip); 
+             if($preparedQuery = $conn->prepare('INSERT into nimbusiouserpayment (uid, cardnumber, nameoncard, expdate, ccissuer, cvv, addressl1, addressl2, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)')){
+                $preparedQuery->bind_param('isssissi', $ccuid, $ccn, $ccun, $cced, $cci, $cccvv, $ccadd1, $ccadd2, $cczip); 
                 if($preparedQuery->execute()){
                     echo 'Card Added!';
                 }
