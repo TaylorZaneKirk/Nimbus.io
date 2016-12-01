be4beaec2c5fbc389388fbdaa2ae535d44893867
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-12-01 08:19:15 -0600

    All results, one array

--- PHP-code/prototype.php
+++ PHP-code/prototype.php
@@ -3,6 +3,8 @@
 
     $uid = $_GET['uid'];
 
+    $result = array();
+
     if($preparedQuery = $conn->prepare('SELECT IPaddress, serverid FROM userservers WHERE uid=?')){
         
         $preparedQuery->bind_param('i', $uid);
@@ -17,9 +19,9 @@
                 $preparedQuery2->bind_result($r_servicename, $r_servicedesc, $r_serviceprice, $r_Memory, $r_Processor, $r_Storage);
 
                 if($preparedQuery2->fetch()){
-                    $result = array();
+                    
                     array_push($result, array("IPaddress"=>$r_IPaddress, "serviceid"=>$r_serverid, "servicename"=>$r_servicename, "servicedesc"=>$r_servicedesc, "serviceprice"=>$r_serviceprice, "Memory"=>$r_Memory, "Processor"=>$r_Processor, "Storage"=>$r_Storage));
-                    echo json_encode(array("result"=>$result));
+                    
                 }
                 $preparedQuery2->close();
              }
@@ -30,4 +32,6 @@
 
         $preparedQuery->close();
     }
+
+    echo json_encode(array("result"=>$result));
 ?>
